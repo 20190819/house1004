75b944ed39985aa27b605587aa5a7d0b6ca9828c
yangliang <mac@123456.com>
2021-10-06 17:39:11 +0800

    captcha srv

--- main.go
+++ main.go
@@ -10,9 +10,7 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		c.Writer.WriteString("go web 项目开始啦 ！")
 	})
-
-	r.GET("/api/v1/image_code/:uuid", controllers.GetImageCd)
-	r.GET("/captcha/:captchaId", controllers.GetImage)
+	r.GET("/api/v1/captcha", controllers.GetCaptcha)
 
 	err := r.Run(":8888")
 	if err != nil {
--- services/captcha/handler/captcha.go
+++ services/captcha/handler/captcha.go
@@ -1,7 +1,10 @@
 package handler
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"github.com/dchest/captcha"
 	pb "house1004/services/captcha/proto"
 )
 
@@ -9,7 +12,13 @@ type Captcha struct{}
 
 func (e *Captcha) Call(ctx context.Context, req *pb.CapRequest) (*pb.CapResponse, error) {
 
+	var content bytes.Buffer
+	id := captcha.NewLen(4)
+	url := fmt.Sprintf("/captcha_img/%s.png", id)
+	captcha.WriteImage(&content, id, captcha.StdWidth, captcha.StdHeight)
 	return &pb.CapResponse{
-		Data: []byte("this is captcha service"),
+		ImgId:   id,
+		ImgUrl:  url,
+		ImgData: content.Bytes(),
 	}, nil
 }
