706b1a7e38a3aa12655a3a8b4d783c8ae344b1ca
杨亮 <yangliang@114study.com>
2021-11-15 09:35:22 +0800

    update

--- web/utils/ftime.go
+++ web/utils/ftime.go
@@ -8,17 +8,17 @@ import (
 
 type Ftime time.Time
 
-var defaultFormat = "2006-01-02 15:04:05"
+const DEFAULT_TIME_FORMAT = "2006-01-02 15:04:05"
 
 func (t Ftime) MarshalJSON() ([]byte, error) {
-	tStr := time.Now().Format(defaultFormat) // 设置格式
+	tStr := time.Now().Format(DEFAULT_TIME_FORMAT) // 设置格式
 	// 注意 json 字符串风格要求
 	return []byte(fmt.Sprintf("\"%v\"", tStr)), nil
 }
 
 func (t Ftime) Value() (driver.Value, error) {
 	// Ftime 转换成 time.Time 类型
-	return time.Now().Format(defaultFormat), nil
+	return time.Now().Format(DEFAULT_TIME_FORMAT), nil
 }
 
 func (t *Ftime) Scan(v interface{}) error {
